1fb9a75ebf8670a189a189997e9d0bcc2522c97a
Alan Meneguim <alan.meneguim@gmail.com>
2019-03-31 17:28:31 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/meneguim/cursomc/CursomcApplication.java
+++ src/main/java/com/meneguim/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.meneguim.cursomc.domain.Cidade;
 import com.meneguim.cursomc.domain.Cliente;
 import com.meneguim.cursomc.domain.Endereco;
 import com.meneguim.cursomc.domain.Estado;
+import com.meneguim.cursomc.domain.ItemPedido;
 import com.meneguim.cursomc.domain.Pagamento;
 import com.meneguim.cursomc.domain.PagamentoComBoleto;
 import com.meneguim.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.meneguim.cursomc.repositories.CidadeRepository;
 import com.meneguim.cursomc.repositories.ClienteRepository;
 import com.meneguim.cursomc.repositories.EnderecoRepository;
 import com.meneguim.cursomc.repositories.EstadoRepository;
+import com.meneguim.cursomc.repositories.ItemPedidoRepository;
 import com.meneguim.cursomc.repositories.PagamentoRepository;
 import com.meneguim.cursomc.repositories.PedidoRepository;
 import com.meneguim.cursomc.repositories.ProdutoRepository;
@@ -48,7 +50,9 @@ public class CursomcApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -111,6 +115,19 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 200.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
 		
 	}
 
--- src/main/java/com/meneguim/cursomc/domain/Pedido.java
+++ src/main/java/com/meneguim/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.meneguim.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -32,7 +35,10 @@ public class Pedido implements Serializable{
 	@ManyToOne
 	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco endereDeEntrega;
-
+	
+	@OneToMany(mappedBy="id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>(); 
+				
 	public Pedido() {
 		super();
 	}
@@ -87,6 +93,14 @@ public class Pedido implements Serializable{
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	@Override
