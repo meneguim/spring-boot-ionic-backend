25ca30d74edf76ffc0bc1411e71aee2e6005feac
Alan Meneguim <alan.meneguim@gmail.com>
2019-03-31 14:13:17 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/meneguim/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.meneguim.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.meneguim.cursomc.domain.Cliente;
+import com.meneguim.cursomc.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	@Autowired	
+	private ClienteRepository repo;
+	
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new com.meneguim.cursomc.services.exceptions.ObjectNotFoundException(
+						"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+}
