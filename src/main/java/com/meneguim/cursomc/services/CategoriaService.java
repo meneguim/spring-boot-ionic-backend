751407c7d058502569139f921a1965272dd98116
Alan Meneguim <alan.meneguim@gmail.com>
2019-04-07 20:57:41 -0300

    Validacao sintatica com Bean validation

--- src/main/java/com/meneguim/cursomc/services/CategoriaService.java
+++ src/main/java/com/meneguim/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.meneguim.cursomc.domain.Categoria;
+import com.meneguim.cursomc.dto.CategoriaDTO;
 import com.meneguim.cursomc.repositories.CategoriaRepository;
 import com.meneguim.cursomc.services.exceptions.DataIntegrityException;
 
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page,Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(),objDTO.getNome());
 	}
 }
